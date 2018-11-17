64b892984d32af19e38b6a5dd8c46c149e7e0dbb
hernandavidc <hernandavidc@hotmail.com>
2018-11-17 17:35:31 -0500

    aceptar/declinar prestamos

--- plataforma/investment/urls.py
+++ plataforma/investment/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import MisOfertasList, OfertarPrueba, MisOfertasRecibidasList
+from .views import ofertaRecibidaDecline, ofertaRecibidaAdd, MisOfertasList, OfertarPrueba, MisOfertasRecibidasList
 
 inversionista_patterns = ([
     path('inversionista/', MisOfertasList.as_view(), name='listOfertas'),
     path('inversionista/<int:solicitud>/', OfertarPrueba.as_view(), name='ofertar'),
+    path('solicitudes/ok/<int:pk>/', ofertaRecibidaAdd, name='add'),
+    path('solicitudes/no/<int:pk>/', ofertaRecibidaDecline, name='decline'),
     path('solicitudes/', MisOfertasRecibidasList.as_view(), name='solicitudes'),
 ],'inversionistas')

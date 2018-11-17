554d73319869675b9205a02c1fd56c3c8f368262
hernandavidc <hernandavidc@hotmail.com>
2018-11-17 04:20:09 -0500

    rr3

--- plataforma/prestamos/views.py
+++ plataforma/prestamos/views.py
@@ -10,7 +10,6 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Solicitudes
 from .forms import SolicitudAdd
 
-
 class SolicitudesList(ListView):
     model = Solicitudes
     template_name = "prestamos/list_solicitudes.html"

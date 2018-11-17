dbb6ddb369e2b2376226dc2481dfca03475d2756
Andrea <a_claro8@hotmail.com>
2018-11-17 05:10:16 -0500

    Lllenado base de datos :)

--- plataforma/prestamos/admin.py
+++ plataforma/prestamos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Solicitudes, Categorias, Parametros, Rankings
+from .models import Solicitudes, Categorias, Parametros, Rankings, NivelesDeRiesgo
 
 admin.site.register(Solicitudes)
 admin.site.register(Categorias)
 admin.site.register(Parametros)
 admin.site.register(Rankings)
+admin.site.register(NivelesDeRiesgo)
